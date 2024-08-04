8dc8bc5323131c18eaa168d35babd8227f2fa26f
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-08-04 13:18:28 +0300

    some new tools to try

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const container = document.querySelector(".container");
 const containerWidth = container.offsetWidth;
 const gridSizeButton = document.querySelector(".grid-size");
+const randomColorButton = document.querySelector(".random-color");
+const opacityButton = document.querySelector(".opacity");
 let gridSize = 16;
+let isColorRandom = false;
+let isOpacityFeatureOn = false;
+let opacity = 1;
 
 function createGrid(n) {
-  const size = calcDivSize(containerWidth, n) + "px";
+  const divSize = calcDivSize(containerWidth, n) + "px";
 
   for (let i = 0; i < n * n; i++) {
     const div = document.createElement("div");
-    div.style.width = size;
-    div.style.height = size;
+    div.style.width = divSize;
+    div.style.height = divSize;
     container.appendChild(div);
   }
 }
@@ -20,9 +25,20 @@ function calcDivSize(width, number) {
   return size;
 }
 
-container.addEventListener("mousemove", function (e) {
+container.addEventListener("mouseover", function (e) {
   const square = e.target;
-  square.style.backgroundColor = "black";
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+  if (isColorRandom) {
+    square.style.backgroundColor = "#" + randomColor;
+  } else {
+    square.style.backgroundColor = "black";
+  }
+
+  if (isOpacityFeatureOn) {
+    container.style.opacity = opacity - 0.1;
+    opacity -= 0.1;
+  }
 });
 
 gridSizeButton.addEventListener("click", function () {
@@ -30,6 +46,14 @@ gridSizeButton.addEventListener("click", function () {
   container.textContent = "";
   size > 100 ? (gridSize = 100) : (gridSize = size);
   createGrid(gridSize);
+});
+
+randomColorButton.addEventListener("click", function () {
+  isColorRandom = true;
+});
+
+opacityButton.addEventListener("click", function () {
+  isOpacityFeatureOn = true;
 });
 
 createGrid(gridSize);
